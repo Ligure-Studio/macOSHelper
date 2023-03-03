b295d5e7d4674962e64877bda54d221ced8364a1
Jerry-XU1010 <xhx101028@outlook.com>
2023-03-04 06:53:08 +0800

    UI进行中……

---
+++ macOSHelper/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macOSHelper
+//
+//  Created by Jerry XU on 2023/2/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
