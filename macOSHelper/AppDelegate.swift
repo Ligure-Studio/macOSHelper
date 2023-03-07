d437b036e2aadc72469f2ac24baa1aefa1f7a1ee
Jerry-XU1010 <xhx101028@outlook.com>
2023-03-07 20:05:51 +0800

    GU

--- macOSHelper/AppDelegate.swift
+++ macOSHelper/AppDelegate.swift
@@ -24,6 +24,34 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
+    
+    @IBAction func 官网(_ sender: NSButton) {
+        print("官网")
+    }
+    
+    @IBAction func 开启全部来源(_ sender: NSButton) {
+        print("打开全部来源")
+    }
+    
+    @IBAction func 关闭全部来源(_ sender: NSButton) {
+        print("关闭全部来源")
+    }
+    
+    @IBAction func 解决已损坏问题(_ sender: NSButton) {
+        print("解决“已损坏”问题")
+    }
+    
+    @IBAction func 复制HomeBrew安装命令(_ sender: NSButton) {
+        print("重置Dock")
+    }
+    
+    @IBAction func 取消四位密码限制(_ sender: NSButton) {
+        print("取消四位密码限制")
+    }
+    
+    @IBAction func 重置Dock(_ sender: NSButton) {
+        print("重置Dock")
+    }
 
 
 }
---
+++ macOSHelper/Func.swift
@@ -0,0 +1,82 @@
+//
+//  Func.swift
+//  macOSHelper
+//
+//  Created by Jerry XU on 2023/3/5.
+//
+
+import Cocoa
+
+public func showOpenPanel() -> String {//打开文件函数,返回值为路径
+    let openPanel = NSOpenPanel()
+    openPanel.allowedFileTypes = ["app"]
+    openPanel.allowsMultipleSelection = false
+    openPanel.canChooseDirectories = false
+    openPanel.canChooseFiles = true
+    if (openPanel.runModal() ==  NSApplication.ModalResponse.OK) {
+        let result = openPanel.url // Pathname of the file
+
+        if (result != nil) {
+            return result!.path
+        }
+    }
+    else {
+        return "failed"
+    }
+    return "failed"
+}
+
+public func runScript(_ command: String) -> String {
+    let task = Process()
+    let pipe = Pipe()
+    
+    task.standardOutput = pipe
+    task.standardError = pipe
+    task.arguments = ["-e", command]
+    task.launchPath = "/usr/bin/osascript"
+    task.standardInput = nil
+    task.launch()
+    
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: .utf8)!
+    
+    return output
+}
+
+class RoundedColoredButton: NSButton {
+    @IBInspectable var bgColor: NSColor = .darkGray
+    @IBInspectable var foreColor: NSColor = .white
+    @IBInspectable var highlightColor: NSColor = .black
+    @IBInspectable var cornerRadius: CGFloat = 8
+    
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+        configure()
+    }
+    
+    func configure() {
+        // Set the proper background color depending on
+        // whether the button is highlighted or not.
+        if !isHighlighted {
+            self.layer?.backgroundColor = bgColor.cgColor
+        } else {
+            self.layer?.backgroundColor = highlightColor.cgColor
+        }
+        
+        // Create an attributed string using the
+        // provided title color, and use that attributed
+        // string as title.
+        let attributedString = NSAttributedString(string: title,
+                                                  attributes: [NSAttributedString.Key.foregroundColor: foreColor])
+        self.attributedTitle = attributedString
+        
+        // Set the corner radius.
+        self.layer?.cornerRadius = cornerRadius
+    }
+}
+
+public func copyStringToPasteboard(string: String) {
+    let pboard = NSPasteboard.general           // 1
+    pboard.declareTypes([.string], owner: nil)  // 2
+    pboard.setString(string, forType: .string)  // 3
+}
