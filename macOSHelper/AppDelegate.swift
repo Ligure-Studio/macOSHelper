7789d7a2e8df900b9a52876c210969061a67dd1d
Jerry-XU1010 <xhx101028@outlook.com>
2023-04-01 16:54:28 +0800

    关闭窗口时自动退出应用

--- macOSHelper/AppDelegate.swift
+++ macOSHelper/AppDelegate.swift
@@ -10,8 +10,9 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    
-
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
